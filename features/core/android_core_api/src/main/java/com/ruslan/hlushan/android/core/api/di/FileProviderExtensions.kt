8fc6cf075ed714c01061069d6e729bd898f07a67
Ruslan Hlushan <37369640+rHlushan@users.noreply.github.com>
2021-08-20 13:16:35 +0300

    improve (#16)

--- features/core/android_core_api/src/main/java/com/ruslan/hlushan/android/core/api/di/FileProviderExtensions.kt
+++ features/core/android_core_api/src/main/java/com/ruslan/hlushan/android/core/api/di/FileProviderExtensions.kt
@@ -1,6 +1,8 @@
 package com.ruslan.hlushan.android.core.api.di
 
+import android.content.ContentResolver
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import androidx.core.content.FileProvider
 import java.io.File
@@ -12,9 +14,34 @@ import java.lang.IllegalStateException
  */
 private val Context.fileProviderAuthority: String get() = "${this.packageName}.fileprovider"
 
-fun Context.createExternalReportsFile(
-        fileName: String,
-): File {
+fun Context.createExternalReportsFileWithReadPermissionsForOtherApps(
+        fileName: String
+): FileWithUri {
+    val file = this.createExternalReportsFile(fileName)
+    val reportUri = this.getUriForFile(file)
+    this.grantUriPermission(this.packageName, reportUri, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+    return FileWithUri(file = file, uri = reportUri)
+}
+
+fun createOpenFileWithReadPermissionForOtherApps(
+        contentResolver: ContentResolver,
+        uri: Uri
+): Intent {
+    val originalIntent = Intent(Intent.ACTION_VIEW)
+            .setDataAndType(uri, contentResolver.getType(uri))
+            .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+    //todo: hardcoded text to resources
+    val chooserIntent = Intent.createChooser(originalIntent, "Select app")
+    chooserIntent.flags = (Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+    return chooserIntent
+}
+
+class FileWithUri(
+        val file: File,
+        val uri: Uri
+)
+
+fun Context.createExternalReportsFile(fileName: String): File {
     val reportsPath = this.getExternalFilesDir("reports")!!
     if (!reportsPath.exists()) {
         reportsPath.mkdirs()
