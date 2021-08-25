2a15001dff5d882017d2ec810285bc77c1df9911
Ruslan Hlushan <37369640+rHlushan@users.noreply.github.com>
2021-08-25 12:11:09 +0300

    improve (#25)

--- buildSrc/src/main/java/ApplicationConfigs.kt
+++ buildSrc/src/main/java/ApplicationConfigs.kt
@@ -1,16 +1,15 @@
-/**
- * @author Ruslan Hlushan on 2019-08-28
- */
+import org.gradle.api.JavaVersion
 
 object ApplicationConfigs {
-    const val minSdkVersion = 21// don't forget about proguard
-    const val targetSdkVersion = 30
-    const val compileSdkVersion = targetSdkVersion
-    const val buildToolsVersion = "30.0.2"
+    const val minSdkVersion: Int = 21// don't forget about proguard
+    const val targetSdkVersion: Int = 30
+    const val compileSdkVersion: Int = targetSdkVersion
+    const val buildToolsVersion: String = "30.0.2"
+    val JAVA_VERSION: JavaVersion = JavaVersion.VERSION_1_8
 
-    const val defaultProguardFile = "proguard-android-optimize.txt"
+    const val defaultProguardFile: String = "proguard-android-optimize.txt"
 
-    const val defaultLanguageNonFullCode = "en"
+    const val defaultLanguageNonFullCode: String = "en"
     val availableLanguagesFullCodes: List<String> = listOf("en_GB", "ru_RU", "fr_FR", "es_ES", "it_IT", "pt_PT", "de_DE", "pl_PL", "be_BY")
     val applicationLanguagesNonFullCodes: List<String> = availableLanguagesFullCodes.map { fullCode -> fullCode.split("_").first() }
 }
