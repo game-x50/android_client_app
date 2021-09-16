6f288ffc79f43aae5569f5981f40bacabbc56b5a
ruslanhlushan <ruslan.hlushen@gmail.com>
2021-09-16 16:13:13 +0300

    works

--- features/core/ui/pagination/view/src/main/java/com/ruslan/hlushan/core/ui/pagination/view/BaseSearchPaginationFragment.kt
+++ features/core/ui/pagination/view/src/main/java/com/ruslan/hlushan/core/ui/pagination/view/BaseSearchPaginationFragment.kt
@@ -1,13 +1,9 @@
 package com.ruslan.hlushan.core.ui.pagination.view
 
-import android.app.SearchManager
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.annotation.ContentView
 import androidx.annotation.LayoutRes
-import androidx.appcompat.widget.SearchView
 import com.ruslan.hlushan.core.extensions.ifNotNull
 import com.ruslan.hlushan.core.thread.UiMainThread
 import com.ruslan.hlushan.core.ui.fragment.BaseFragment
@@ -21,18 +17,18 @@ constructor(
 
     protected abstract val viewModel: VM
 
-    protected abstract val searchView: SearchView
+//    protected abstract val searchView: SearchView
 
-    override fun onNewIntent(intent: Intent?) {
-        super.onNewIntent(intent)
-
-        ifNotNull(intent) { nonNullIntent ->
-            if (Intent.ACTION_SEARCH == nonNullIntent.action) {
-                val query = nonNullIntent.getStringExtra(SearchManager.QUERY)
-                searchView.setQuery(query, true)
-            }
-        }
-    }
+//    override fun onNewIntent(intent: Intent?) {
+//        super.onNewIntent(intent)
+//
+//        ifNotNull(intent) { nonNullIntent ->
+//            if (Intent.ACTION_SEARCH == nonNullIntent.action) {
+//                val query = nonNullIntent.getStringExtra(SearchManager.QUERY)
+//                searchView.setQuery(query, true)
+//            }
+//        }
+//    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -42,24 +38,24 @@ constructor(
 
     @UiMainThread
     private fun setSearchViewWatcher() = ifNotNull(activity) { nonNullActivity ->
-        val searchManager = nonNullActivity.getSystemService(Context.SEARCH_SERVICE) as? SearchManager
-        if (searchManager != null) {
-            val searchableInfo = searchManager.getSearchableInfo(nonNullActivity.componentName)
-            searchView.setSearchableInfo(searchableInfo)
-        }
-        searchView.setIconifiedByDefault(false)
-        searchView.isIconified = false
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextChange(searchString: String): Boolean {
-                viewModel.setSearchText(searchString)
-                return true
-            }
-
-            override fun onQueryTextSubmit(s: String): Boolean {
-                hideKeyboard()
-                return true
-            }
-        })
-        searchView.clearFocus()
+//        val searchManager = nonNullActivity.getSystemService(Context.SEARCH_SERVICE) as? SearchManager
+//        if (searchManager != null) {
+//            val searchableInfo = searchManager.getSearchableInfo(nonNullActivity.componentName)
+//            searchView.setSearchableInfo(searchableInfo)
+//        }
+//        searchView.setIconifiedByDefault(false)
+//        searchView.isIconified = false
+//        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+//            override fun onQueryTextChange(searchString: String): Boolean {
+//                viewModel.setSearchText(searchString)
+//                return true
+//            }
+//
+//            override fun onQueryTextSubmit(s: String): Boolean {
+//                hideKeyboard()
+//                return true
+//            }
+//        })
+//        searchView.clearFocus()
     }
 }
