cd5c0ab94a8d7315b90736f1085060f84b3ba12e
ruslanhlushan <ruslan.hlushen@gmail.com>
2021-10-28 14:13:38 +0300

    Fix dependency issue

--- features/core/api/lib/src/main/java/com/ruslan/hlushan/core/api/dto/DatabaseViewInfo.kt
+++ features/core/api/lib/src/main/java/com/ruslan/hlushan/core/api/dto/DatabaseViewInfo.kt
@@ -1,5 +1,7 @@
 package com.ruslan.hlushan.core.api.dto
 
+//todo: move to separate package and
+// recheck dependency graph where DatabaseViewInfo is used
 data class DatabaseViewInfo(
         val clazz: Class<*>,
         val name: String
